7ca2d2bbb1cd943c2e517fe70d574a374ee0cb74
jatinthakuurrr <jt1123276@gmail.com>
2026-09-14 01:31:17 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,1 @@
+const express = require('express'); const app = express(); const PORT = process.env.PORT || 3000; app.use(express.json()); app.get('/', (req, res) => { res.send('Auto Lister V2 Active'); }); app.listen(PORT, () => { console.log(Server running on port ${PORT}); });
